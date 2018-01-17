a7fa1ab01f14de5c1e8a38d32d68b567ba0c29ff
zhangbiao <2570886020@qq.com>
2018-01-17 19:54:56 +0800

    修改乱码过滤器

--- src/com/san/filter/EncodingFilter.java
+++ src/com/san/filter/EncodingFilter.java
@@ -11,6 +11,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.net.httpserver.Filter.Chain;
+
 public class EncodingFilter implements Filter{
 
 	public void destroy() {
@@ -19,13 +21,13 @@ public class EncodingFilter implements Filter{
 	}
 	//简单的解决中文乱码问题
 	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain filter) throws IOException, ServletException {
+			FilterChain chain) throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		HttpServletRequest req=(HttpServletRequest) request;
 		HttpServletResponse resp=(HttpServletResponse) response;
 		req.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html;charset=utf-8");
-		doFilter(req, resp, filter);
+		chain.doFilter(req, resp);
 	}
 
 	public void init(FilterConfig arg0) throws ServletException {
