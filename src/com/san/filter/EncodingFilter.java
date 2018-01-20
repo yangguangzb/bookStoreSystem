1060755162c5398ef49e94fda1f85eac99794192
zhangbiao <2570886020@qq.com>
2018-01-20 16:44:45 +0800

    修改编码问题

--- src/com/san/filter/EncodingFilter.java
+++ src/com/san/filter/EncodingFilter.java
@@ -11,7 +11,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.net.httpserver.Filter.Chain;
 
 public class EncodingFilter implements Filter{
 
