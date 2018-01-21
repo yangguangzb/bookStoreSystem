569ef8cac557a62755c54ae03f7f542a81545a0e
zhangbiao <2570886020@qq.com>
2018-01-21 22:32:57 +0800

    C3p0

--- src/com/san/util/C3P0Util.java
+++ src/com/san/util/C3P0Util.java
@@ -10,12 +10,9 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 public class C3P0Util {
 	//得到一个数据源
 		private static DataSource dataSource = new ComboPooledDataSource();
-		
-		
 		public static DataSource getDataSource() {
 			return dataSource;
 		}
-
 		//从数据源中得到一个连接对象
 		public static Connection getConnection(){
 			try {
@@ -27,29 +24,29 @@ public class C3P0Util {
 		
 		public static void release(Connection conn,Statement stmt,ResultSet rs){
 			//关闭资源
-					if(rs!=null){
-						try {
-							rs.close();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						rs = null;
-					}
-					if(stmt!=null){
-						try {
-							stmt.close();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						stmt = null;
-					}
-					if(conn!=null){
-						try {
-							conn.close();//关闭
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-						conn = null;
-					}
+			if(rs!=null){
+				try {
+					rs.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				rs = null;
+			}
+			if(stmt!=null){
+				try {
+					stmt.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				stmt = null;
+			}
+			if(conn!=null){
+				try {
+					conn.close();//关闭
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				conn = null;
+			}
 		}
 }
