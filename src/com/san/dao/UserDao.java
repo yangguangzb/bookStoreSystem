7281e61c36404ff26279548df925cf081e1e8cbf
zhangbiao <2570886020@qq.com>
2018-02-27 21:52:37 +0800

    用户dao

--- src/com/san/dao/UserDao.java
+++ src/com/san/dao/UserDao.java
@@ -1,15 +1,11 @@
 package com.san.dao;
 import java.sql.SQLException;
-
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
-
 import com.san.domain.User;
 import com.san.util.C3P0Util;
-
-
-
 public class UserDao {
+	//添加用户
 	public void addUser(User user) throws SQLException {
 		QueryRunner qr = new QueryRunner(C3P0Util.getDataSource());
 		String sql = "INSERT INTO USER(username,PASSWORD,gender,email,telephone,introduce,activecode,state,registtime) "
@@ -18,23 +14,19 @@ public class UserDao {
 				user.getGender(), user.getEmail(), user.getTelephone(),
 				user.getIntroduce(), user.getActiveCode(), user.getState(),
 				user.getRegistTime());
-
 	}
-
 	// 根据激活码查找用户
 	public User findUserByActiveCode(String activeCode) throws SQLException {
 		QueryRunner qr = new QueryRunner(C3P0Util.getDataSource());
 		return qr.query("select * from user where activecode=?",
 				new BeanHandler<User>(User.class), activeCode);
 	}
-
 	// 修改用户激活状态
 	public void activeCode(String activeCode) throws SQLException {
 		QueryRunner qr = new QueryRunner(C3P0Util.getDataSource());
 		qr.update("update user set state=1 where activecode=?", activeCode);
 
 	}
-
 	// 用户登录
 	public User findUserByUserNameAndPassword(String username, String password)
 			throws SQLException {
@@ -54,6 +46,15 @@ public class UserDao {
 	public void modifyUser(User user) throws SQLException {
 		QueryRunner qr = new QueryRunner(C3P0Util.getDataSource());
 		qr.update("update user set password=? , gender=?, telephone=? where id=?",user.getPassword(),user.getGender(),user.getTelephone(),user.getId());
-		
+	}
+	//通过激活码查找用户
+	public User findUserByactionCode(String actionCode) throws SQLException {
+		QueryRunner qr=new QueryRunner(C3P0Util.getDataSource());
+		return qr.query("select * from user where activeCode=?",new BeanHandler<User>(User.class),actionCode);
+	}
+	//激活用户(更新用户状态)
+	public void activeUser(String actionCode) throws SQLException {
+		QueryRunner qr=new QueryRunner(C3P0Util.getDataSource());
+		qr.update("update user set state=1 where activeCode=?",actionCode);
 	}
 }
